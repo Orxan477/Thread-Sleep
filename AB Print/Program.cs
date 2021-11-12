55440ef68f761095a78730f3e4dcebbe76fc3601
Orxan Qanbarov <41814449+Orxan477@users.noreply.github.com>
2021-11-12 12:18:30 +0400

    A and B Print Threat Finished

---
+++ AB Print/Program.cs
@@ -0,0 +1,34 @@
+﻿
+using System;
+using System.Threading;
+
+namespace AB_Print
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread thread1 = new Thread(PrintA);
+            Thread thread2 = new Thread(PrintB);
+            thread1.Start();
+            thread2.Start();
+        }
+        static void PrintA()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine($"A");
+            }
+        }
+        static void PrintB()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Thread.Sleep(2100);
+                Console.WriteLine($"B");
+            }
+        }
+
+    }
+}
